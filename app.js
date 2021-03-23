20ad4f168a622e17ab49d7a03ee08b493c7eb783
sai-koushik-macha <saikoushikmacha@gmail.com>
2021-03-23 18:09:28 +0530

    Added views/landing.ejs and Changing Home Page to landing.ejs
    app.get("/", function(req, res){
        res.render("landing");
    });

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express")
 var app = express()
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.send("This will be landing page");
+    res.render("landing");
 });
 
 app.listen(3000, function(){
