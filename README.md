f11dc332840ec8d5a0b5d9ce61c151e8ea31f7d8
sai-koushik-macha <saikoushikmacha@gmail.com>
2021-05-25 10:08:02 +0530

    Add register logic
    add form for signing up
    add signing up logic using passport in app.js

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Add User Model
+# Add User model
 * Install all packages need for auth
 * Define User Model
+
+# Register
+* Configure Passport
+* Add register routes
+* Add register template
--- app.js
+++ app.js
@@ -22,6 +22,22 @@ app.use(bodyPaser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// passport configuration
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -120,6 +136,34 @@ app.post("/campgrounds/:id/comments", function(req, res){
     });
     
 });
+
+
+// =============
+// AUTH ROUTES
+// =============
+
+// show register form
+app.get("/register", function(req, res) {
+   res.render("register"); 
+});
+
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/campgrounds");
+            });
+        }
+        
+    });
+});
+
+
 
 app.listen(3000, function(){
     console.log("The YelpCamp Server has started!");
